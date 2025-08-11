8cad215e1395ec4cc4b7097eba35783473536dbb
Victor Zornek <vzornek@gmail.com>
2025-08-11 11:37:38 -0300

    add new structure for initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { MdBusiness, MdMail } from 'react-icons/md';
 import { Container } from "./styles";
 
 import { ButtonLinktree } from "@/components/ButtonLinktree";
+import { Header } from "@/components/Header";
 
 import logoTizeck from '@/assets/LogoTizeck.png'
 
@@ -12,7 +13,18 @@ import logoTizeck from '@/assets/LogoTizeck.png'
 export default function HomePage() {
     return(
         <Container>
-            
+            <Header />
+
+            <main>
+                <section className="hero">
+                    <div className="wrapper-text">
+                        <h1>Excelência em Produtos</h1>
+
+                        <p>Há mais de 20 anos no mercado, oferecemos soluções de qualidade com compromisso e inovação</p>
+                    </div>
+
+                </section>
+            </main>
         </Container>
     )
 }
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -3,7 +3,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 37.5rem;
+    width: 39.5rem;
     height: 100vh;
 
     display: flex;
@@ -11,16 +11,38 @@ export const Container = styled.div`
 
     margin: 2rem auto;
 
-    > h1 {
-        font-size: 4.8rem;
-        text-align: center;
+    > main {
+        margin-top: 3rem;
 
-        margin-bottom: 6rem;
+        .hero {
+            height: 40rem;
+
+            margin: 0 auto;
+            text-align: center;
+
+            background: linear-gradient(135deg,rgba(16, 26, 51, 1) 0%, rgba(59, 129, 245, 1) 100%);
+
+            .wrapper-text {
+                padding: 5rem 3rem 0;
+                margin-bottom: 3rem;
+
+                display: flex;
+                flex-direction: column;
+                gap: 2rem;
+
+                h1 {
+                    font-size: 3rem;
+                    font-weight: ${({ theme }) => theme.FONTS_WEIGHT.BOLD};
+                }
+
+                p {
+                    font-size: 1.2rem;
+                }
+            }
+        }
     }
 
-    .wrapper-buttons {
-        display: flex;
-        flex-direction: column;
-        gap: 1.5rem;
-    }
+
+
+
 `
