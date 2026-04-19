4db1f63ba7b453f0e22424ba25fb4dfeea0b66ee
Victor Zornek <vzornek@gmail.com>
2026-04-19 03:01:53 -0300

    chore: repositioning logo into hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function HomePage() {
             <main>
                 <section className="hero">
                     <div className="wrapper-text">
-                        <Image src={logoTizeck} alt="Logo Tizeck" width={400} height={160} priority style={{ margin: '0 auto 1rem' }} />
+                        <Image src={logoTizeck} alt="Logo Tizeck" width={400} height={160} priority style={{ margin: '-10rem auto 0' }} />
                         <h1>Excelência em Produtos</h1>
 
                         <p>Há mais de 20 anos no mercado, oferecemos soluções de qualidade com compromisso e inovação</p>
